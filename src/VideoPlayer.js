52bb536600ca05597a7bc6911eb5b2f801b4214f
ShivanshSharma1 <shivanshs331@gmail.com>
2020-11-09 16:22:53 +0530

    allowed embed player to play full screen

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -5,7 +5,7 @@ const VideoPlayer = ({ url }) => {
     <div style={{height:'1000px',}}>
         <div>
             <div  class="embed-responsive embed-responsive-16by9" style={{width:'75%',margin:'auto', position:'relative', top:'100px',}}>
-                <iframe class="embed-responsive-item" src={`https://www.youtube.com/embed/${url}?rel=0`} title="Song's Video" ></iframe>
+                <iframe class="embed-responsive-item" src={`https://www.youtube.com/embed/${url}?rel=0`} title="Song's Video" allowfullscreen="true"></iframe>
             </div>
         </div>
     </div>
